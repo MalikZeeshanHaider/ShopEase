fa329febfb4e4d7f530c78c112475a0b55caab39
MalikZeeshanHaider <221093@students.au.edu.pk>
2024-11-05 19:40:48 +0500

    Add Order Service with Dockerfile

---
+++ order-service/Application.java
@@ -0,0 +1,21 @@
+package com.example.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+@RestController
+class OrderController {
+    @GetMapping("/")
+    public String home() {
+        return "{\"message\": \"Order Service is running!\"}";
+    }
+}
